731de0cf66b649e55f09a29f059e6b08c4d13307
Your Name <you@example.com>
2025-05-11 19:41:12 +0200

    creation data_base OK

--- exercice a faire/Exercice1_find-simple.md
+++ exercice a faire/Exercice1_find-simple.md
@@ -2,7 +2,7 @@
 
 Dans une base de données 'exercice' :
 
-1. Importer les données console_games.json
+1. Importer les données .json
 2. Afficher les jeux 3DS sortis
 3. Afficher les jeux 3DS sortis en 2011
 4. Afficher le nom et le global_sales des jeux 3DS sortis en 2011
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from MongoManager import MongoManager
 from config.db_config import DB_NAME, MONGO_URI
+from services.exercices import exo_1
 
 def main():
-    # Initialize MongoManager with the URI and database name
+    """# Initialize MongoManager with the URI and database name
     mongo_manager = MongoManager(MONGO_URI, DB_NAME)
 
     # List all databases
@@ -11,7 +12,9 @@ def main():
     for db in databases:
         print(f"- {db}")
     # Close the connection when done
-    mongo_manager.close_connection()
+    mongo_manager.close_connection()"""
+    exo_1()
+
 
 if __name__ == "__main__":
     main()
